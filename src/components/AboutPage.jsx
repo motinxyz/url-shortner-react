93282594bd87944081dbe9e96b8f30502da8703e
motinxyz <motinsghs@gmail.com>
2025-03-16 21:32:48 +0600

    minor fix

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { FaLink, FaShareAlt, FaEdit, FaChartLine } from "react-icons/fa";
 const AboutPage = () => {
   return (
-    <div className=" lg:px-14 sm:px-8 px-5 min-h-[calc(100vh-64px)] pt-2">
+    <div className="lg:px-14 sm:px-8 px-5 min-h-[calc(100vh-64px)] pt-2">
       <div className="bg-white w-full sm:py-10 py-8  ">
-        <h1 className=" sm:text-4xl text-slate-800 text-3xl font-bold italic  mb-3">
+        <h1 className=" sm:text-4xl text-slate-800 text-3xl font-bold italic  mb-1">
           About UrlShortner
         </h1>
+        {/* <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" /> */}
+        <hr class="w-auto h-1 mx-auto mt-3 bg-gray-100 border-0 rounded-sm md:my-5 dark:bg-gray-400"></hr>
         <p className="text-gray-700 text-sm  mb-8 xl:w-[60%] lg:w-[70%] sm:w-[80%] w-full ">
           UrlShortner simplifies URL shortening for efficient sharing. Easily
-          generate, manage, and track your shortened links. UrlShortner simplifies
-          URL shortening for efficient sharing. Easily generate, manage, and
-          track your shortened links. UrlShortner simplifies URL shortening for
+          generate, manage, and track your shortened links. UrlShortner
+          simplifies URL shortening for efficient sharing. Easily generate,
+          manage, and track your shortened links. UrlShortner simplifies URL
+          shortening for efficient sharing. Easily generate, manage, and track
+          your shortened links. UrlShortner simplifies URL shortening for
           efficient sharing. Easily generate, manage, and track your shortened
-          links. UrlShortner simplifies URL shortening for efficient sharing.
-          Easily generate, manage, and track your shortened links.
+          links.
         </p>
         <div className="space-y-5 xl:w-[60%] lg:w-[70%] sm:w-[80%] w-full ">
           <div className="flex itemsr-start">
